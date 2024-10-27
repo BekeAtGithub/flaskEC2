e5b78b11b5d55326259c40da1f335e94af5a2179
Adam Marton Beke <65732867+BekeAtGithub@users.noreply.github.com>
2024-10-27 15:14:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # main.tf
 
 provider "aws" {
-  region = "us-east-1"  # Update to your preferred region
+  region = "us-east-1"  # Update to your preferred AWS region
 }
 
 # Variables
@@ -9,21 +9,58 @@ variable "instance_count" {
   default = 2  # Number of EC2 instances to deploy
 }
 
-# Retrieve the default VPC
-data "aws_vpc" "default" {
-  default = true
+# Create a custom VPC
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "CustomVPC"
+  }
 }
 
-# Create a new subnet within the default VPC
-resource "aws_subnet" "default_subnet" {
-  vpc_id                  = data.aws_vpc.default.id
-  cidr_block              = "10.0.1.0/24"  # Choose an unused CIDR within the VPC range
-  availability_zone       = "us-east-1a"   # Update to an availability zone in your region
+# Create a subnet in the custom VPC
+resource "aws_subnet" "custom_subnet" {
+  vpc_id                  = aws_vpc.custom_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"   # Adjust to your desired availability zone
   map_public_ip_on_launch = true
+  tags = {
+    Name = "CustomSubnet"
+  }
+}
+
+# Create an Internet Gateway for the VPC
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.custom_vpc.id
+  tags = {
+    Name = "CustomIGW"
+  }
+}
+
+# Create a route table for the VPC
+resource "aws_route_table" "custom_route_table" {
+  vpc_id = aws_vpc.custom_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "CustomRouteTable"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.custom_subnet.id
+  route_table_id = aws_route_table.custom_route_table.id
 }
 
 # Create a security group to allow HTTP access
 resource "aws_security_group" "app_sg" {
+  vpc_id      = aws_vpc.custom_vpc.id
   name        = "app_security_group"
   description = "Allow HTTP inbound traffic"
 
@@ -32,7 +69,7 @@ resource "aws_security_group" "app_sg" {
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from all IPs
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -54,15 +91,15 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-# Launch EC2 instances in the newly created subnet
+# Launch EC2 instances in the custom subnet
 resource "aws_instance" "app_instance" {
   count         = var.instance_count
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.default_subnet.id
+  subnet_id     = aws_subnet.custom_subnet.id
   security_groups = [aws_security_group.app_sg.name]
 
-  # User data script to install Docker, run Flask app
+  # User data script to install Docker and run Flask app
   user_data = <<-EOF
               #!/bin/bash
               amazon-linux-extras install docker -y
